9f20cf513c434b9b740659a3f18b5557b95caad1
Khalil Gayle <118006546+fullstackkg@users.noreply.github.com>
2023-12-02 20:41:25 -0500

    Updated flexbox and error handling

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ async function retrieveImage() {
     try {
         const response = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=galaxy")
         const responseData = await response.json()
-        console.log(responseData)
 
         const authorName = responseData.user.name
         const image = responseData.urls.full
+        console.log(image)
         document.querySelector("#author").innerText = `By: ${authorName}`
         document.body.style.backgroundImage = `url("${image}")`
 
     } catch (e) {
-        console.error(e)
+        document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1528818955841-a7f1425131b5?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDE1Njc1NjN8&ixlib=rb-4.0.3&q=85")`
     }
 }
